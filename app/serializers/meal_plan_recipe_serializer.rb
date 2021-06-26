00155b571abe431591e4a52ab11537ab77138003
Roman Turner <rn.turner@live.com>
2021-06-25 19:01:56 -0700

    trying without the serializer

--- app/serializers/meal_plan_recipe_serializer.rb
+++ app/serializers/meal_plan_recipe_serializer.rb
@@ -1,17 +1,17 @@
 class MealPlanRecipeSerializer < ActiveModel::Serializer
   attributes :id, :recipe, :meal_plan
 
-  def meal_plan 
-    object.meal_plan.id,
-    object.meal_plan.unassigned,
-    object.meal_plan.monday,
-    object.meal_plan.tuesday,
-    object.meal_plan.wednesday,
-    object.meal_plan.thursday,
-    object.meal_plan.friday,
-    object.meal_plan.saturday,
-    object.meal_plan.sunday,
-  end
+  # def meal_plan 
+  #   object.meal_plan.id,
+  #   object.meal_plan.unassigned,
+  #   object.meal_plan.monday,
+  #   object.meal_plan.tuesday,
+  #   object.meal_plan.wednesday,
+  #   object.meal_plan.thursday,
+  #   object.meal_plan.friday,
+  #   object.meal_plan.saturday,
+  #   object.meal_plan.sunday,
+  # end
   
   def meal_plan_recipes
     object.meal_plan_recipes.map do |mpr|
